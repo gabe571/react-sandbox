80e977cafc0c626265afd5059ac1e028f39113a1
Gabriel Castro <gabriel.castro1028@gmail.com>
2022-04-17 16:25:20 -0700

    counter console logging

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,22 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import React, { useState } from 'react'
 
-export default function Home() {
 
-
+const Counter = () => {
   const [count, setCount] = useState(0);
-  const [countDown, setCountDown] = useState(0);
-  const [countReset, setCountReset] = useState(0);
+
+  const increaseAndLog = () => {
+    setCount(count + 1)
+    console.log('ayyyyyeee console and updating')
+  }
 
   return (
     <div>
-     <button onClick={() => setCount(count + 1)}> Up {count} </button>
-     <br></br>
-     <button onClick={()=> setCountDown(countDown - 1)}> Down {countDown}</button>
-     <br></br>
-     <button onClick={() => setCountReset(countReset(0))}> Reset </button>
+      <button onClick={increaseAndLog}>  {count}</button>
+      <br/>
+      <button onClick={() => setCount(count - 1)}>  {count}</button>
     </div>
   )
 }
+
+
+export default Counter
